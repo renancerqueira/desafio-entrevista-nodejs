2d3ef92449f96564f50fc38b5868cb96c9ba9366
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-10-21 17:33:39 -0300

    chore: new vehicle migration

---
+++ src/database/typeorm/migrations/1666383898892-CreateVehicle.ts
@@ -0,0 +1,99 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+  TableIndex,
+} from 'typeorm';
+
+export class CreateVehicle1666383898892 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'vehicle',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'vehicle_type_id',
+            type: 'uuid',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'brand',
+            type: 'varchar',
+          },
+          {
+            name: 'model',
+            type: 'varchar',
+          },
+          {
+            name: 'color',
+            type: 'varchar',
+          },
+          {
+            name: 'license_plate',
+            type: 'varchar',
+          },
+          {
+            name: 'passenger_capacity',
+            type: 'int',
+          },
+          {
+            name: 'is_active',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            default: null,
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'vehicle',
+      new TableForeignKey({
+        name: 'fk-vehicle-vehicle_type_id',
+        columnNames: ['vehicle_type_id'],
+        referencedTableName: 'vehicle_type',
+        referencedColumnNames: ['id'],
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createIndex(
+      'vehicle',
+      new TableIndex({
+        name: 'fk-vehicle-vehicle_type_id',
+        columnNames: ['vehicle_type_id'],
+        parser: 'btree',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('vehicle');
+  }
+}
