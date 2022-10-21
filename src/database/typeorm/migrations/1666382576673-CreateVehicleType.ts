2b3e976f6417a3100210bb114e3f37dd8227a91d
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-10-21 17:27:05 -0300

    fix: fix the migration filename

--- src/database/typeorm/migrations/1666382576673-CreateVehicleType.ts
+++ src/database/typeorm/migrations/1666382576673-CreateVehicleType.ts
@@ -1,8 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateCompanyVacancyType1666382576673
-  implements MigrationInterface
-{
+export class CreateVehicleType1666382576673 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
