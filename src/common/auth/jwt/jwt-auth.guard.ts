6db6e0dec87f1718b3a440aa7844983223f2498c
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-10-23 23:53:48 -0300

    chore: add schema to validation and decorators for auth

---
+++ src/common/auth/jwt/jwt-auth.guard.ts
@@ -0,0 +1,35 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+
+import { IS_PUBLIC_KEY } from '@common/decorators/public.decorator';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      console.log(JwtAuthGuard.name, { err, user, info });
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
---
+++ src/common/auth/jwt/jwt.strategy.ts
@@ -0,0 +1,43 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import requestIp from 'request-ip';
+
+import { AuthService } from '@app/auth/auth.service';
+
+export type JwtData = {
+  id: string;
+  user_agent: string;
+  ip: string;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('jwt.secret') || 'secret',
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(request: Request, payload: any): Promise<JwtData> {
+    const id = payload.sub;
+    const userExists = await this.authService.validateUser(id);
+    if (!userExists) {
+      throw new UnauthorizedException();
+    }
+
+    return {
+      id,
+      user_agent: request.headers['user-agent'],
+      ip: requestIp.getClientIp(request),
+    };
+  }
+}
