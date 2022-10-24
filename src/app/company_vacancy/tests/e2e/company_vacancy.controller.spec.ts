80c8f40e72e9cfba124c5346e323af0648bf151c
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-10-23 21:59:36 -0300

    test: company vacancy test e2e

---
+++ src/app/company_vacancy/tests/e2e/company_vacancy.controller.spec.ts
@@ -0,0 +1,220 @@
+import { faker } from '@faker-js/faker';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import dayjs from 'dayjs';
+import request from 'supertest';
+import { DataSource } from 'typeorm';
+
+import { Company } from '@app/company/entities/company.entity';
+import { CompanyFakerBuilder } from '@app/company/tests/faker-builder/company-faker-builder';
+import { CompanyVacancyRepository } from '@app/company_vacancy/company_vacancy.repository';
+import { VehicleType } from '@app/vehicle_type/entities/vehicle_type.entity';
+import { VehicleTypeFakerBuilder } from '@app/vehicle_type/tests/faker-builder/vehicle_type-faker-builder';
+import {
+  clearRepositories,
+  createNestApplication,
+} from '@common/helpers/test-helpers';
+import { validateUUIDV4 } from '@common/utils/validate-uuid-v4';
+
+import { CompanyVacancyFakerBuilder } from '../faker-builder/company_vacancy-faker-builder';
+
+describe('Company Vacancy - /company-vacancies (e2e)', () => {
+  let app: INestApplication;
+  let dbConnection: DataSource;
+  let repository: CompanyVacancyRepository;
+  let company: Company;
+  let vehicleType: VehicleType;
+
+  beforeAll(async () => {
+    app = await createNestApplication({
+      onBeforeInit: (moduleRef) => {
+        dbConnection = moduleRef.get(DataSource);
+        repository = moduleRef.get(CompanyVacancyRepository);
+      },
+    });
+  });
+
+  beforeEach(async () => {
+    await clearRepositories(dbConnection);
+    const companyPayload = CompanyFakerBuilder.aCompany().build();
+    company = await dbConnection.manager.save(Company, companyPayload);
+
+    const vehicleTypePayload = VehicleTypeFakerBuilder.aVehicleType()
+      .withName('Car')
+      .build();
+    vehicleType = await dbConnection.manager.save(
+      VehicleType,
+      vehicleTypePayload,
+    );
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it(`/GET company-vacancies (empty result)`, async () => {
+    const response = await request(app.getHttpServer())
+      .get('/company-vacancies')
+      .send()
+      .expect(HttpStatus.OK);
+
+    expect(response.body).toEqual([]);
+  });
+
+  it(`/GET company-vacancies`, async () => {
+    const payload = CompanyVacancyFakerBuilder.aCompanyVacancy()
+      .withCompanyId(company.id)
+      .withVehicleTypeId(vehicleType.id)
+      .withQuantity(20)
+      .build();
+    await repository.save(payload);
+
+    const { body } = await request(app.getHttpServer())
+      .get('/company-vacancies')
+      .expect(HttpStatus.OK);
+
+    expect(validateUUIDV4(body[0].id)).toBeTruthy();
+    expect(company.id).toEqual(body[0].company.id);
+    expect(company.social_name).toEqual(body[0].company.social_name);
+    expect(company.fantasy_name).toEqual(body[0].company.fantasy_name);
+    expect(company.email).toEqual(body[0].company.email);
+
+    expect(vehicleType.id).toEqual(body[0].vehicleType.id);
+    expect(vehicleType.name).toEqual(body[0].vehicleType.name);
+
+    expect(body[0].quantity).toEqual(payload.quantity);
+    expect(body[0].is_active).toEqual(payload.is_active);
+    expect(body[0].created_at).toEqual(payload.created_at.toISOString());
+    expect(body[0].updated_at).toEqual(payload.updated_at.toISOString());
+  });
+
+  it(`/POST company-vacancies (empty response)`, async () => {
+    const payload = CompanyVacancyFakerBuilder.aCompanyVacancy()
+      .withCompanyId(company.id)
+      .withVehicleTypeId(vehicleType.id)
+      .withQuantity(30)
+      .build();
+
+    const response = await request(app.getHttpServer())
+      .post('/company-vacancies')
+      .send(payload)
+      .expect(HttpStatus.CREATED);
+
+    expect(response.body).toEqual({});
+  });
+
+  it(`/POST company-vacancies (UnprocessableEntityException: Vacancy already registered)`, async () => {
+    const payload = CompanyVacancyFakerBuilder.aCompanyVacancy()
+      .withCompanyId(company.id)
+      .withVehicleTypeId(vehicleType.id)
+      .withQuantity(20)
+      .build();
+
+    await request(app.getHttpServer())
+      .post('/company-vacancies')
+      .send(payload)
+      .expect(HttpStatus.CREATED);
+
+    const response = await request(app.getHttpServer())
+      .post('/company-vacancies')
+      .send(payload)
+      .expect(HttpStatus.UNPROCESSABLE_ENTITY);
+
+    expect(response.body).toStrictEqual({
+      statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      message: 'Vacancy already registered',
+      error: 'Unprocessable Entity',
+    });
+  });
+
+  it(`/PATCH company-vacancies`, async () => {
+    const payload = CompanyVacancyFakerBuilder.aCompanyVacancy()
+      .withCompanyId(company.id)
+      .withVehicleTypeId(vehicleType.id)
+      .withQuantity(20)
+      .build();
+
+    await request(app.getHttpServer())
+      .post('/company-vacancies')
+      .send(payload)
+      .expect(HttpStatus.CREATED);
+
+    const created = await request(app.getHttpServer())
+      .get('/company-vacancies')
+      .expect(HttpStatus.OK);
+
+    expect(validateUUIDV4(created.body[0].id)).toBeTruthy();
+    expect(created.body[0].quantity).toEqual(expect.any(Number));
+    expect(created.body[0].quantity).toEqual(20);
+    expect(created.body[0].is_active).toEqual(payload.is_active);
+    expect(dayjs(created.body[0].created_at).isValid()).toBeTruthy();
+    expect(dayjs(created.body[0].updated_at).isValid()).toBeTruthy();
+
+    const { id } = created.body[0];
+    await request(app.getHttpServer())
+      .patch(`/company-vacancies/${id}`)
+      .send({ quantity: 30 })
+      .expect(HttpStatus.NO_CONTENT);
+
+    const updatedCompany = await request(app.getHttpServer())
+      .get(`/company-vacancies/${id}`)
+      .expect(HttpStatus.OK);
+
+    expect(updatedCompany.body.quantity).toEqual(30);
+    expect(dayjs(updatedCompany.body.updated_at).isValid()).toBeTruthy();
+
+    expect(created.body[0].id).toEqual(updatedCompany.body.id);
+    expect(created.body[0].is_active).toEqual(updatedCompany.body.is_active);
+    expect(created.body[0].created_at).toEqual(updatedCompany.body.created_at);
+  });
+
+  it(`/PATCH company-vacancies (NotFoundException: Vacancy not found)`, async () => {
+    const response = await request(app.getHttpServer())
+      .patch(`/company-vacancies/${faker.datatype.uuid()}`)
+      .send({ quantity: 10 })
+      .expect(HttpStatus.NOT_FOUND);
+
+    expect(response.body).toStrictEqual({
+      statusCode: HttpStatus.NOT_FOUND,
+      message: 'Vacancy not found',
+      error: 'Not Found',
+    });
+  });
+
+  it(`/DELETE company-vacancies`, async () => {
+    const payload = CompanyVacancyFakerBuilder.aCompanyVacancy()
+      .withCompanyId(company.id)
+      .withVehicleTypeId(vehicleType.id)
+      .withQuantity(20)
+      .build();
+
+    await request(app.getHttpServer())
+      .post('/company-vacancies')
+      .send(payload)
+      .expect(HttpStatus.CREATED);
+
+    const response = await request(app.getHttpServer())
+      .get('/company-vacancies')
+      .send(payload)
+      .expect(HttpStatus.OK);
+
+    const { id } = response.body[0];
+
+    const deletedCompany = await request(app.getHttpServer())
+      .delete(`/company-vacancies/${id}`)
+      .expect(HttpStatus.NO_CONTENT);
+
+    expect(deletedCompany.body).toEqual({});
+  });
+
+  it(`/DELETE company-vacancies (NotFoundException: Vacancy not found)`, async () => {
+    const response = await request(app.getHttpServer())
+      .delete(`/company-vacancies/${faker.datatype.uuid()}`)
+      .expect(HttpStatus.NOT_FOUND);
+
+    expect(response.body).toStrictEqual({
+      statusCode: HttpStatus.NOT_FOUND,
+      message: 'Vacancy not found',
+      error: 'Not Found',
+    });
+  });
+});
