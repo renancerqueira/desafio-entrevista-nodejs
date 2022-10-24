762ac02d5e524708cb140ce71e85c5d85187b305
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-10-23 22:20:09 -0300

    chore: new create vacancy migration

---
+++ src/database/typeorm/migrations/1666573409642-CreateVacancy.ts
@@ -0,0 +1,109 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+  TableIndex,
+} from 'typeorm';
+
+export class CreateVacancy1666573409642 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'vacancy',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'company_id',
+            type: 'varchar',
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'vehicle_id',
+            type: 'varchar',
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'date_in',
+            type: 'datetime',
+          },
+          {
+            name: 'date_out',
+            type: 'datetime',
+          },
+          {
+            name: 'is_active',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            default: null,
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'vacancy',
+      new TableForeignKey({
+        name: 'fk-vacancy-company_id',
+        columnNames: ['company_id'],
+        referencedTableName: 'company',
+        referencedColumnNames: ['id'],
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'vacancy',
+      new TableForeignKey({
+        name: 'fk-vacancy-vehicle_id',
+        columnNames: ['vehicle_id'],
+        referencedTableName: 'vehicle',
+        referencedColumnNames: ['id'],
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createIndex(
+      'vacancy',
+      new TableIndex({
+        name: 'fk-vacancy-company_id',
+        columnNames: ['company_id'],
+        parser: 'btree',
+      }),
+    );
+
+    await queryRunner.createIndex(
+      'vacancy',
+      new TableIndex({
+        name: 'fk-vacancy-vehicle_id',
+        columnNames: ['vehicle_id'],
+        parser: 'btree',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('vacancy');
+  }
+}
