5647f37dd4670e2ac5600cd9168cb4e65c8731c2
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-10-24 11:39:47 -0300

    chore: removed wrong relationship

--- src/app/vehicle_type/entities/vehicle_type.entity.ts
+++ src/app/vehicle_type/entities/vehicle_type.entity.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, JoinTable, OneToOne, Relation } from 'typeorm';
 
 import { CompanyVacancy } from '@app/company_vacancy/entities/company_vacancy.entity';
+import { Vehicle } from '@app/vehicle/entities/vehicle.entity';
 import { BaseEntity } from '@common/entity';
 
 @Entity('vehicle_type')
@@ -13,6 +14,11 @@ export class VehicleType extends BaseEntity {
   @ApiProperty()
   @Column({ default: true })
   is_active?: boolean;
+
+  @ApiProperty()
+  @OneToOne(() => Vehicle, (vehicle) => vehicle.vehicleType)
+  @JoinTable()
+  vehicle: Relation<Vehicle[]>;
 
   @ApiProperty()
   @OneToOne(
